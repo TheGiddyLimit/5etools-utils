faf1e7317430f03238c34310fd6efe576c2ff37d
TheGiddyLimit <TheGiddyLimit@5etools.com>
2022-09-29 21:43:48 +0100

    chore: use woke terminology

--- test/TestUtilFs.js
+++ test/TestUtilFs.js
@@ -2,5 +2,5 @@ import {listJsonFiles} from "../lib/UtilFs.js";
 
 // TODO an actual test framework
 
-const results = listJsonFiles("./test/data/json", {dirBlacklist: new Set(["./test/data/json/block"])})
+const results = listJsonFiles("./test/data/json", {dirBlocklist: new Set(["./test/data/json/block"])})
 if (results.length !== 2) throw new Error();
