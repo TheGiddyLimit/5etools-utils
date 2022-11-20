b8e6bcffe6f1e2d93782ddccff9d5f0571752030
TheGiddyLimit <TheGiddyLimit@5etools.com>
2022-11-20 17:20:33 +0000

    feat(json): add multi-threaded validation

--- test/TestJson.test.js
+++ test/TestJson.test.js
@@ -3,11 +3,12 @@ import {JsonTester } from "../lib/TestJson.js";
 test(
 	"Test JSON schema tester",
 	() => {
-		const {errors} = new JsonTester({
+		const {errors, errorsFull} = new JsonTester({
 			dirSchema: "test/data/test-test-json/schema",
 			fnGetSchemaId: () => "schema.json",
 		}).getErrors("test/data/test-test-json/data");
 
 		expect(errors.length).toBe(3);
+		expect(errorsFull.length).toBe(3);
 	},
 );
