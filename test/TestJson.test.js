c643c85fa3d9a8c36672e2f645c201bdecdb5263
TheGiddyLimit <TheGiddyLimit@5etools.com>
2024-05-14 11:13:09 +0100

    feat(json): correctly handle remote schema URLs

--- test/TestJson.test.js
+++ test/TestJson.test.js
@@ -2,13 +2,32 @@ import {JsonTester} from "../lib/TestJson.js";
 
 test(
 	"Test JSON schema tester",
-	() => {
-		const {errors, errorsFull} = new JsonTester({
+	async () => {
+		const jsonTester = new JsonTester({
 			dirSchema: "test/data/test-test-json/schema",
 			fnGetSchemaId: () => "schema.json",
-		}).getErrors("test/data/test-test-json/data");
+		});
+		await jsonTester.pInit();
+
+		const {errors, errorsFull} = jsonTester.getErrors("test/data/test-test-json/data");
 
 		expect(errors.length).toBe(3);
 		expect(errorsFull.length).toBe(3);
+	},
+);
+
+test(
+	"Test JSON schema tester (brew)",
+	async () => {
+		const jsonTester = new JsonTester({
+			mode: "brew",
+			fnGetSchemaId: () => "homebrew.json",
+		});
+		await jsonTester.pInit();
+
+		const {errors, errorsFull} = jsonTester.getErrors("test/data/test-test-json/data-brew");
+
+		expect(errors.length).toBe(0);
+		expect(errorsFull.length).toBe(0);
 	},
 );
