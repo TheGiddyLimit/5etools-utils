2dfdc25ed7f0062a1f922711ae0ee5cb7086193e
TheGiddyLimit <TheGiddyLimit@5etools.com>
2026-02-14 20:49:55 +0000

    test(source): update dump; add legacy/classic/modern defs

--- node/fetch-5etools-sources.js
+++ node/fetch-5etools-sources.js
@@ -9,8 +9,8 @@ Use with caution.
 
 */
 
-import fs from "fs";
 import path from "path";
+import {writeJsonSync} from "../lib/UtilFs.js";
 
 const _TEMPLATE = {
 	"$schema": "https://json-schema.org/draft/2020-12/schema",
@@ -25,17 +25,30 @@ const _TEMPLATE = {
 
 async function main () {
 	// :^)
-	// eslint-disable-next-line no-eval
+	/* eslint-disable no-eval */
 	eval(await (await fetch(`https://raw.githubusercontent.com/5etools-mirror-3/5etools-src/refs/heads/main/js/parser.js`)).text());
+	eval(await (await fetch(`https://raw.githubusercontent.com/5etools-mirror-3/5etools-src/refs/heads/main/js/utils.js`)).text());
+	/* eslint-enable no-eval */
+
+	const sources = Object.keys(Parser.SOURCE_JSON_TO_FULL);
 	_TEMPLATE.$defs.sources = {
 		"type": "string",
-		"enum": Object.keys(Parser.SOURCE_JSON_TO_FULL),
+		"enum": sources,
 	};
-	fs.writeFileSync(
-		path.join("schema-template", "sources-5etools.json"),
-		JSON.stringify(_TEMPLATE, null, "\t"),
-		"utf-8",
-	);
+	_TEMPLATE.$defs.sourcesLegacy = {
+		"type": "string",
+		"enum": sources.filter(src => SourceUtil.isLegacySourceWotc(src)),
+	};
+	_TEMPLATE.$defs.sourcesClassic = {
+		"type": "string",
+		"enum": sources.filter(src => SourceUtil.isClassicSource(src)),
+	};
+	_TEMPLATE.$defs.sourcesModern = {
+		"type": "string",
+		"enum": sources.filter(src => !SourceUtil.isClassicSource(src)),
+	};
+
+	writeJsonSync(path.join("schema-template", "sources-5etools.json"), _TEMPLATE, {isClean: true});
 }
 
 main();
